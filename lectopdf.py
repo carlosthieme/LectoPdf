829ceca9dd6b439c0f01c241062984461056efe9
Carlos Thieme <97850884+carlosthieme@users.noreply.github.com>
2022-01-23 17:07:06 -0300

    Add files via upload

---
+++ lectopdf.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        Principal
+# Purpose:     Leer un archivo pdf y guiardarlo en formato csv.
+#
+# Author:      caarlos
+#
+# Created:     22-01-2022
+# Copyright:   (c) caarlos 2022
+# Licence:     <free2use>
+#-------------------------------------------------------------------------------
+import pdfplumber
+
+
+
+i = 0
+archivo=open("path_to_file.txt","w")
+
+while(i <= 6):
+    with pdfplumber.open("path_toFile.pdf") as pdf_file:
+        pdf_page = pdf_file.pages[i]
+        archivo.write(pdf_page.extract_text())
+        archivo.write("\n")
+    i+=1
+archivo.close()
+print("Terminado")
+
